3cbfc9c9bd8eb13d0d24e3eb7d1da318336fbbd4
Unknown <krypt-lynx@yandex.ru>
2020-10-02 20:31:19 +0200

    RWLayout for settings

--- Source/ModDiff.cs
+++ Source/ModDiff.cs
@@ -2,8 +2,10 @@
 // PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
 
 using HarmonyLib;
+using RWLayout.moddiff;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Text;
@@ -32,16 +34,57 @@ namespace ModDiff
         }
     }
 
-    public class ModDiff : Mod
+
+    public abstract class CMod : Mod // todo: update local RWLayout lib
+    {
+        public CMod(ModContentPack content) : base(content) { }
+
+        CGuiRoot gui = null;
+        public CGuiRoot Gui
+        {
+            get
+            {
+                if (gui == null)
+                {
+                    gui = new CGuiRoot();
+                    ConstructGui();
+                }
+                return gui;
+            }
+        }
+
+        public override void WriteSettings()
+        {
+            base.WriteSettings();
+
+            gui = null; // this method is called after settings window close
+        }
+
+        public virtual void ConstructGui() { }
+
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
+            base.DoSettingsWindowContents(inRect);
+
+            var settingsGui = Gui;
+            settingsGui.InRect = inRect;
+            settingsGui.UpdateLayoutIfNeeded();
+            settingsGui.DoElementContent();
+        }
+    }
+
+
+    public class ModDiff : CMod
     {
         public static string PackageIdOfMine = null;
         public static Settings Settings { get; private set; }
+        public static string CommitInfo = null;
 
         public static bool CassowaryPackaged = true;
 
         public ModDiff(ModContentPack content) : base(content)
         {
-            PackageIdOfMine = content.PackageId;
+            ReadModInfo(content);
             Settings = GetSettings<Settings>();
 
             Harmony harmony = new Harmony(PackageIdOfMine);
@@ -51,11 +94,67 @@ namespace ModDiff
 
         }
 
+        private static void ReadModInfo(ModContentPack content)
+        {
+            PackageIdOfMine = content.PackageId;
+
+            var name = Assembly.GetExecutingAssembly().GetName().Name;
+
+            try
+            {
+                using (Stream stream = Assembly.GetExecutingAssembly()
+                        .GetManifestResourceStream(name + ".git.txt"))
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    CommitInfo = reader.ReadToEnd()?.TrimEndNewlines();
+                }
+            }
+            catch
+            {
+                CommitInfo = null;
+            }
+        }
+
         public override string SettingsCategory()
         {
             return "Mod Diff";
         }
 
+
+        public override void ConstructGui()
+        {
+            Gui.StackTop(StackOptions.Create(intrinsicIfNotSet: true, constrainEnd: false),
+             Gui.AddElement(new CCheckBox
+             {
+                Title = "IgnoreSelfTitle".Translate(),
+                Checked = Settings.ignoreSelf,
+                 Changed = (_, value) => Settings.ignoreSelf = value,
+             }), 2,
+             Gui.AddElement(new CCheckBox
+             {
+                 Title = "KeepSelfLoaded".Translate(),
+                 Checked = Settings.selfPreservation,
+                 Changed = (_, value) => Settings.selfPreservation = value,
+             }), 2,
+             Gui.AddElement(new CCheckBox
+             {
+                 Title = "AlternativePalette".Translate(),
+                 Checked = Settings.alternativePallete,
+                 Changed = (_, value) => Settings.alternativePallete = value,
+             })
+            );
+
+            Gui.StackBottom(StackOptions.Create(intrinsicIfNotSet: true, constrainEnd: false),
+                Gui.AddElement(new CLabel
+                {
+                    Title = CommitInfo,
+                    TextAlignment = TextAnchor.LowerRight,
+                    Color = new Color(1, 1, 1, 0.5f),
+                    Font = GameFont.Tiny
+                })
+            );
+        }
+        /*
         public override void DoSettingsWindowContents(Rect inRect)
         {
             base.DoSettingsWindowContents(inRect);
@@ -71,7 +170,7 @@ namespace ModDiff
 
             options.End();
         }
-
+        */
 
     }
 
